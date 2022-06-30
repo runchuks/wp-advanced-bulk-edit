72af006619d0b4d2b11e5fa706d33cb1a50906d6
Jānis Veiss <janis.veiss93@gmail.com>
2022-06-30 15:16:36 +0300

    Update

--- wp-advanced-bulk-edit.php
+++ wp-advanced-bulk-edit.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: WordPress Advanced Bulk Edit
- * Version: 1.0.0
+ * Version: 1.0.1
  * Plugin URI: http://www.greenwiremedia.com/
  * Description: WordPress Advanced Bulk Edit
  * Author: JV@GWM
